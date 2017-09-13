853f3c9dad94ab69788eac2f17be702cf2811fae
Trong Van <trongvan214@yahoo.com>
2017-09-13 15:01:48 -0500

    possible solution

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-$(function(){
+$(document).ready(function(){
+    // $(document).on('click', function(event){
+    //     var clickOver = $(event.target);
+    //     var opened = $('#menu-icon').hasClass('menu-active');
+    //     if(clickOver.is('#menu-icon')){
+    //         $('nav').toggleClass('menu-active');
+    //     }
+    //     else if(opened === true && !clickOver.hasClass("media-active")) {
+    //         $("#menu-icon").click();
+    //     }
+    // });
     $('#menu-icon').on('click', function(){
         $(this).toggleClass('menu-active');
     });
