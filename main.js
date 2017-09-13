5f5b30dc89eb3d9f913256e448c55d2239b2348d
Trong Van <trongvan214@yahoo.com>
2017-09-13 11:04:53 -0500

    second attempt to fix bug

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 $(function(){
-    $(document).on('click', function(event){
-        var clickOver = $(event.target);
-        var opened = $('#menu-icon').hasClass('menu-active');
-        if(clickOver.is('#menu-icon')){
-            $('#menu-icon').toggleClass('menu-active');
-        }
-        else if(opened === true && !clickOver.hasClass("media-active")) {
-            $("#menu-icon").click();
-        }
-    });
+    if(screen.width >= 768){
+        $(document).on('click', function(event){
+            var clickOver = $(event.target);
+            var opened = $('#menu-icon').hasClass('menu-active');
+            if(clickOver.is('#menu-icon')){
+                $('#menu-icon').toggleClass('menu-active');
+            }
+            else if(opened === true && !clickOver.hasClass("media-active")) {
+                $("#menu-icon").click();
+            }
+        });
+    }
+    else {
+        $('#menu-icon').on('click', function(){
+            $(this).toggleClass('menu-active');
+        });
+    }
     $('a[href^="#"]').on('click', function(e){
         e.preventDefault();
         $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
