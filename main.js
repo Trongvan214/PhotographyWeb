7689d45980e3ce599981b56240b2f77269cc4933
Trong Van <trongvan214@yahoo.com>
2017-09-13 15:17:40 -0500

    possible solution

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
     $('#menu-icon').on('click', function(){
         $(this).toggleClass('menu-active');
     });
-    $('a[href^="#"]').on('click', function(e){
-        e.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
-    });
+    // $('a[href^="#"]').on('click', function(e){
+    //     e.preventDefault();
+    //     $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+    // });
 });
