2086bc616cfa36ff20e454e500e3f84a1c1ef22e
Trong Van <trongvan214@yahoo.com>
2017-09-13 22:14:26 -0500

    responsive javascript download on media

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-    // $(document).on('click', function(event){
-    //     var clickOver = $(event.target);
-    //     var opened = $('#menu-icon').hasClass('menu-active');
-    //     if(clickOver.is('#menu-icon')){
-    //         $('nav').toggleClass('menu-active');
-    //     }
-    //     else if(opened === true && !clickOver.hasClass("media-active")) {
-    //         $("#menu-icon").click();
-    //     }
-    // });
-    $('#menu-icon').on('click', function(){
-        $(this).toggleClass('menu-active');
+    if(screen.width >= 1080){
+        $(document).on('click', function(event){
+            var clickOver = $(event.target);
+            var opened = $('#menu-icon').hasClass('menu-active');
+            if(clickOver.is('#menu-icon')){
+                $('#menu-icon').toggleClass('menu-active');
+            }
+            else if(opened === true && !clickOver.hasClass("media-active")) {
+                $("#menu-icon").click();
+            }
+        });
+    }
+    else {
+        $('#menu-icon').on('click', function(){
+            $(this).toggleClass('menu-active');
+        });
+    }
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
     });
-    // $('a[href^="#"]').on('click', function(e){
-    //     e.preventDefault();
-    //     $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
-    // });
 });
